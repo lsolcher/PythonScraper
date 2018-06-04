4c4a49b41e2be54dc6bab883a95812cd90742949
Lucas <s0539248@htw-berlin.de>
2018-06-04 23:21:38 +0200

    Implement ZeitScraper

--- lib/ZeitScraper.py
+++ lib/ZeitScraper.py
@@ -11,76 +11,93 @@ import re
 import logging
 import os.path
 
+def scrape(dirpath):
 
-dirpath = os.path.abspath(os.path.dirname(__file__))
-print(dirpath)
-
-logger = logging.getLogger('myapp')
-logfileDir = os.path.join(dirpath, 'logs\\')
-if not os.path.exists(logfileDir):
-    os.makedirs(logfileDir)
-logfile = os.path.join(logfileDir, "log.log")
-hdlr = logging.FileHandler(logfile)
-formatter = logging.Formatter('%(asctime)s %(levelname)s %(message)s')
-hdlr.setFormatter(formatter)
-logger.addHandler(hdlr) 
-logger.setLevel(logging.INFO)
-
-# Websites to scrape
-
-
-article_links = []
-# get recent articles
-URL = 'https://www.zeit.de/politik/index'
-PAGE = urlopen(URL)
-soup_mainpage = BeautifulSoup(PAGE, 'html.parser')
-for link in soup_mainpage.findAll('a', attrs={'href': re.compile('^https://www.zeit.de/politik/')}):
-    if '.html' in link['href'] and '-a-' in link['href']: # eliminate non-articles
+    logger = logging.getLogger('root')
+    logger.info('start scraping zeit')
+    # Websites to scrape
+    
+    
+    article_links = []
+    # get recent articles
+    URL = 'https://www.zeit.de/politik/index'
+    PAGE = urlopen(URL)
+    soup_mainpage = BeautifulSoup(PAGE, 'html.parser')
+    for link in soup_mainpage.findAll('a', attrs={'href': re.compile('^https://www.zeit.de/politik/')}):
+        #print(link['href'])
+        #if '.html' in link['href'] and '-a-' in link['href']: # eliminate non-articles
         article_links.append(link['href'])
         logger.info('found recent article: ' + link['href'])
-    
-"""
-article_links = []
-for iteration in range(0, 10): 
-    try:
-        if iteration > 0:
-            nextUrlTag = soup_mainpage.find('a', href=True, text='Mehr Artikel')
-            URL ='http://www.spiegel.de' + nextUrlTag['href']
-            SPON_PAGE = urlopen(URL)
-            soup_mainpage = BeautifulSoup(SPON_PAGE, 'html.parser')
-        for link in soup_mainpage.findAll('a', attrs={'href': re.compile("^(?!http://.*$).*")}):
-            if '.html' in link['href'] and '-a-' in link['href']: #eliminate non-articles
-                article_links.append('http://www.spiegel.de' + link['href'])
-    except Exception:
-        logger.exception("Error in fetching links")
-
-
-article_links = list(set(article_links)) # eliminate duplicate entries
-
-spon_links = os.path.join(dirpath, "sponLinks.txt") 
-linkFile = open(spon_links, 'w+')   
-for link in article_links:
-    linkFile.write("%s\n" % link)
+        
+    article_links = list(set(article_links)) # eliminate duplicate entries
+    zeitLinks = os.path.join(dirpath, "zeitLinks.txt") 
     
     
-# get article text and save it to file
-print(len(article_links))
-for idx, article in enumerate(article_links):
-    try:
-        thisArticle = []
-        article_url = urlopen(article)
-        soup_article = BeautifulSoup(article_url, 'html.parser')
-        for text in soup_article.findAll('p'):
-            thisArticle.append(text.getText())
-        fileId = 'articles\\SPON_' + str(idx) + '.txt'
-        fileName = os.path.join(dirpath, fileId)
-        sponFile = open(fileName, 'w+', encoding='utf-8') 
-        sponFile.write("%s\n" % thisArticle)
-        sponFile.close
-    except Exception:
-        logger.exception("Error in parsing")
-                
-"""  
+    linkFile = open(zeitLinks, 'w+')   
+    for link in article_links:
+        linkFile.write("%s\n" % link)
+        
+        
+    # get article text and save it to file
+    print(len(article_links))
+    for idx, article in enumerate(article_links):
+        try:
+            thisArticle = []
+            article_url = urlopen(article)
+            soup_article = BeautifulSoup(article_url, 'html.parser')
+            for text in soup_article.findAll('p'):
+                thisArticle.append(text.getText())
+            fileId = 'articles\\ZEIT\\ZEIT_' + str(idx) + '.txt'
+            fileName = os.path.join(dirpath, fileId)
+            sponFile = open(fileName, 'w+', encoding='utf-8') 
+            sponFile.write("%s\n" % thisArticle)
+            sponFile.close
+        except Exception:
+            logger.exception("Error in parsing")    
+    
+    
+    """
+    article_links = []
+    for iteration in range(0, 10): 
+        try:
+            if iteration > 0:
+                nextUrlTag = soup_mainpage.find('a', href=True, text='Mehr Artikel')
+                URL ='http://www.spiegel.de' + nextUrlTag['href']
+                SPON_PAGE = urlopen(URL)
+                soup_mainpage = BeautifulSoup(SPON_PAGE, 'html.parser')
+            for link in soup_mainpage.findAll('a', attrs={'href': re.compile("^(?!http://.*$).*")}):
+                if '.html' in link['href'] and '-a-' in link['href']: #eliminate non-articles
+                    article_links.append('http://www.spiegel.de' + link['href'])
+        except Exception:
+            logger.exception("Error in fetching links")
+    
+    
+    article_links = list(set(article_links)) # eliminate duplicate entries
+    
+    spon_links = os.path.join(dirpath, "sponLinks.txt") 
+    linkFile = open(spon_links, 'w+')   
+    for link in article_links:
+        linkFile.write("%s\n" % link)
+        
+        
+    # get article text and save it to file
+    print(len(article_links))
+    for idx, article in enumerate(article_links):
+        try:
+            thisArticle = []
+            article_url = urlopen(article)
+            soup_article = BeautifulSoup(article_url, 'html.parser')
+            for text in soup_article.findAll('p'):
+                thisArticle.append(text.getText())
+            fileId = 'articles\\SPON_' + str(idx) + '.txt'
+            fileName = os.path.join(dirpath, fileId)
+            sponFile = open(fileName, 'w+', encoding='utf-8') 
+            sponFile.write("%s\n" % thisArticle)
+            sponFile.close
+        except Exception:
+            logger.exception("Error in parsing")
+                    
+    """  
     
 
  
