f2b4ed168472ac3e61acb78c864b4a9c9b8b0d5a
Lucas <s0539248@htw-berlin.de>
2018-06-15 22:22:39 +0200

    add jf scraper

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='Scraper',
+    version='',
+    packages=['lib'],
+    url='',
+    license='',
+    author='Luc',
+    author_email='',
+    description=''
+)
